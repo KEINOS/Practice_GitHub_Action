6c20dca4939f23eefc3741cf54f0874587bf7d1d
KEINOS <github@keinos.com>
2020-03-26 15:59:59 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
-echo 'Env data are:'
+echo 'Env data are are:'
 env
 echo "SUPER_SECRET: ${super_secret:-not found}"
 echo "SUPER_SECRETS: ${super_secrets:-not found}"
