4e0e24c63ac4f2313739b288478a63bf9d51b245
KEINOS <github@keinos.com>
2020-03-28 20:24:05 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,18 @@
     echo 'No MY_SECRET_VALUE ENV variable set. Please set your access token to MY_SECRET_VALUE env variable.'
     exit 1
 }
+[ "${GITHUB_REPOSITORY:+dummy}" ] || {
+    echo 'No GITHUB_REPOSITORY ENV variable set. Please set your GitHub repository name to GITHUB_REPOSITORY env variable. [Format username/reponame]'
+    exit 1
+}
+[ "${GITHUB_WORKSPACE:+dummy}" ] || {
+    echo 'No GITHUB_WORKSPACE ENV variable set. Please set GITHUB_WORKSPACE env variable of your path to pull the git repo. [Format /github/workspace]'
+    exit 1
+}
+
+$url_repo_git="https://github.com/${GITHUB_REPOSITORY}.git"
+git clone $url_repo_git $GITHUB_REPOSITORY
 
 php /app/action.php "$@"
+
+ls -la "${GITHUB_REPOSITORY}"
